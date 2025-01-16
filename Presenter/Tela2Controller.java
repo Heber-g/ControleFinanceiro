223200eb48f29e7a7164b054d65fbee7e8ae62dd
Héber <heber_garcia@ufms.br>
2025-01-16 14:11:44 -0400

    Feat: Ajustada a navegação entre telas

--- Presenter/Tela2Controller.java
+++ Presenter/Tela2Controller.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,12 +12,12 @@ import javafx.stage.Stage;
 public class Tela2Controller {
     public void voltarParaTelaInicial(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("TelaInicial.fxml"));
-            Parent root = loader.load();
+            Parent novaTela = FXMLLoader.load(getClass().getResource("/View/telaInicial.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-            // Obtém o Stage atual e muda a Scene
-            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
+            Scene scene = stage.getScene();
+            scene.setRoot(novaTela);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
